f3a01d499ecb8a5397ec52f833caf10a10f2406e
hastmu <gh-hastmu@gmx.de>
2024-08-11 08:48:03 +0200

    update docu

--- .update.readme.sh
+++ .update.readme.sh
@@ -3,5 +3,11 @@
 cd doc
 for item in *.plantuml
 do
-   plantuml ${item}
+   #plantuml ${item}
+   java -jar ~/.vscode/extensions/jebbs.plantuml-*/plantuml.jar ${item}
+   if [ $? -eq 0 ]
+   then
+      git add ${item}
+      git add ${item%.*}.png
+   fi
 done
