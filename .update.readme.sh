a30b0069945bef21389aec206ba5b375940f34cb
hastmu <gh-hastmu@gmx.de>
2024-08-13 23:12:53 +0200

    update process with wip:restore

--- .update.readme.sh
+++ .update.readme.sh
@@ -4,9 +4,11 @@ cd doc
 for item in *.plantuml
 do
    #plantuml ${item}
+   echo "- rendering...${item}"
    java -jar ~/.vscode/extensions/jebbs.plantuml-*/plantuml.jar ${item}
    if [ $? -eq 0 ]
    then
+      echo "+ adding"
       git add ${item}
       git add ${item%.*}.png
    fi
