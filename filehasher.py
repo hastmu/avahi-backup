891637d6ed5c1ea6aa3f5bac378a095c0e1bddc0
hastmu <gh-hastmu@gmx.de>
2025-01-15 21:43:19 +0100

    removed dev statements

--- filehasher.py
+++ filehasher.py
@@ -575,11 +575,11 @@ elif args.remote_patching == True:
       ssh.connect(args.remote_hostname, username=args.remote_username, pkey=pkey, look_for_keys=False)
    else:
       ssh.connect(args.remote_hostname, username=args.remote_username, password=args.remote_password)
-   ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("/home/loc_adm/Syncthing/src/avahi-backup/filehasher.py --version")
+   ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("filehasher.py --version")
    remote_version=ssh_stdout.readline().strip()
    if remote_version == version:
       with open(FH.hashfile,"rb") as handle:
-         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("/home/loc_adm/Syncthing/src/avahi-backup/filehasher.py --inputfile "+args.remote_src_filename+" --min-chunk-size "+str(FH.chunk_size)+" --verify-against - --remote-delta")
+         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("filehasher.py --inputfile "+args.remote_src_filename+" --min-chunk-size "+str(FH.chunk_size)+" --verify-against - --remote-delta")
          ssh_stdin.write(handle.read())
 
          patch_data=FH.receive_msg(pipe=ssh_stdout)
