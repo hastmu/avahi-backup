e6887c6dc14abd1e4b18ce5d41520894b3dcaa00
hastmu <gh-hastmu@gmx.de>
2025-02-09 09:53:18 +0100

    more general functions and lastok state

--- avahi-backup.d/func.hashing.sh
+++ avahi-backup.d/func.hashing.sh
@@ -8,11 +8,12 @@
 declare -A HASH_DATA
 declare -A HASHER_CFG
 declare -A HASHER_QUEUE
+declare -A HASHER_DELTA_LAST_OK
 
 HASHER_QUEUE_FILE=""
 
-HASHER_CFG["FILEHASHER"]="/export/disk-1/home/loc_adm/Syncthing/src/avahi-backup/filehasher.py"
-#HASHER_CFG["FILEHASHER"]="filehasher.py"
+#HASHER_CFG["FILEHASHER"]="/export/disk-1/home/loc_adm/Syncthing/src/avahi-backup/filehasher.py"
+HASHER_CFG["FILEHASHER"]="filehasher.py"
 
 HASHER_CFG["local.version"]="$("${HASHER_CFG["FILEHASHER"]}" --version)"
 
@@ -39,12 +40,20 @@ function hash.save.queue() {
 
    if [ ! -z "${HASHER_QUEUE_FILE}" ]
    then
-      output "- save hasher queue[${HASHER_QUEUE_FILE}]"
-      declare -p HASHER_QUEUE
+      #output "- save hasher queue[${HASHER_QUEUE_FILE}]"
+      #declare -p HASHER_QUEUE
       declare -p HASHER_QUEUE > "${HASHER_QUEUE_FILE}"
    else
       output "! call hash.save.queue without definition of queue file."
    fi
+}
+
+function hash.add2queue() {
+   # $1 ... name
+   # $2 ... chunk size
+   # $3 ... hashfile
+   HASHER_QUEUE["${1}"]="{ 'chunk-size': ${2}, 'hashfile': '${3}' }"
+   hash.save.queue
 }
 
 function hash.local_file() {
@@ -84,8 +93,7 @@ function hash.local_file() {
       hash.save.queue
       return 0
    else
-      HASHER_QUEUE["${3}"]="{ 'chunk-size': $2, 'hashfile': '${hashfile}' }"
-      hash.save.queue
+      hash.add2queue "${3}" "${2}" "${hashfile}"
       output "  ! local hashes are not up-to-date. add to queue."
       return 1
    fi
@@ -136,8 +144,6 @@ function hash.revisit.queue() {
 
          chunk_size="$(echo "${HASHER_QUEUE[${item}]//\'/\"}" | jq -r '."chunk-size"')"
          hash_file="$(echo "${HASHER_QUEUE[${item}]//\'/\"}" | jq -r '."hashfile"')"
-         echo "  - chunk-size: ${chunk_size}"
-         echo "  - hash_file: ${hash_file}"
 
          # remove missing items
          if [ ! -e "${item}" ]
@@ -191,6 +197,45 @@ function hash.remote_file() {
 
 }
 
+
+function hash.lastok.delta() {
+
+   # $1 ... file
+   # $2 ... age limit
+   if [ ${#HASHER_DELTA_LAST_OK[@]} -eq 0 ]
+   then
+      if [ -e "${RUNTIME["BACKUP_ROOT"]}/backup.avahi/hashes/.cache" ]
+      then
+         if source "${RUNTIME["BACKUP_ROOT"]}/backup.avahi/hashes/.cache"
+         then
+            :
+         else
+            rm -f "${RUNTIME["BACKUP_ROOT"]}/backup.avahi/hashes/.cache"
+         fi
+      fi
+   fi
+
+   local -i age=0
+   local -i ret=0
+   local hash_file_name="${RUNTIME["BACKUP_ROOT"]}/backup.avahi/hashes/${1//\//_}"
+   if [ -n "${HASHER_DELTA_LAST_OK["${hash_file_name}"]}" ]
+   then
+      age=$(( $(date +%s) - "${HASHER_DELTA_LAST_OK["${hash_file_name}"]}" ))
+      if [ ${age} -gt ${2} ]
+      then
+         ret=1
+      else
+         ret=0
+      fi
+   else
+      age=-1
+      ret=1
+   fi
+   output "- last-ok age: ${age} sec -> $(( age / 60 )) min"
+   return ${ret}
+
+}
+
 function hash.transfer_remote_file() {
    # $1 ... local file
    # $2 ... chunk-size
@@ -205,39 +250,75 @@ function hash.transfer_remote_file() {
    # 1. checks
    # 1.1. local file hashing complete?
    local -i stat=0
+   local -A pstat
+   # create if not there
+   if [ ! -e "${1}" ]
+   then
+      touch "${1}"
+   fi
+   # hash
    if hash.local_file "10s" "${2}" "${1}" || [ ! -e "${1}" ]
    then
 #      echo "- local hashing complete..."
-      # 1.2. check remote version
-      if remote.hasher.version "${4}"
+      if ! hash.lastok.delta "${1}" "$(( 24 * 60 * 60 ))"
       then
-         export FILEHASHER_SKIP_VERSION=1
-#        echo "- version match"
-         # 2. copy hash to remote - done via stdin
-         # 3. gen patch set
-         timeout 60s "${HASHER_CFG["FILEHASHER"]}" \
-            "--min-chunk-size=${2}" \
-            --inputfile "${1}" \
-            --hashfile "${RUNTIME["BACKUP_ROOT"]}/backup.avahi/hashes/${1//\//_}" \
-            --remote-patching \
-            --remote-host "${4}" \
-            --remote-username "$(id -un)" \
-            --remote-ssh-key ".ssh/backup" \
-            --remote-src-file "${3}" 
-         stat=$?
-         echo "stat: $?"
-         if [ ${stat} -eq 0 ]
+         # 1.2. check remote version
+         if remote.hasher.version "${4}"
          then
-            return 0
-         else
-            return 1
-         fi
+            export FILEHASHER_SKIP_VERSION=1
+   #        echo "- version match"
+            # 2. copy hash to remote - done via stdin
+            # 3. gen patch set
+            echo timeout --preserve-status 60s "${HASHER_CFG["FILEHASHER"]}" \
+               "--min-chunk-size=${2}" \
+               --inputfile "${1}" \
+               --hashfile "${RUNTIME["BACKUP_ROOT"]}/backup.avahi/hashes/${1//\//_}" \
+               --remote-patching \
+               --remote-host "${4}" \
+               --remote-username "$(id -un)" \
+               --remote-ssh-key ".ssh/backup" \
+               --remote-src-file "${3}"
 
+            timeout --preserve-status 60s "${HASHER_CFG["FILEHASHER"]}" \
+               "--min-chunk-size=${2}" \
+               --inputfile "${1}" \
+               --hashfile "${RUNTIME["BACKUP_ROOT"]}/backup.avahi/hashes/${1//\//_}" \
+               --remote-patching \
+               --remote-host "${4}" \
+               --remote-username "$(id -un)" \
+               --remote-ssh-key ".ssh/backup" \
+               --remote-src-file "${3}" | tee "${RUNTIME["BACKUP_ROOT"]}/backup.avahi/hashes/${1//\//_}.log" 2>&1
+            declare -p PIPESTATUS
+            stat=$?
+            echo "stat: $?"
+            local -i unchanged=0
+            unchanged=$(grep -ci unchanged "${RUNTIME["BACKUP_ROOT"]}/backup.avahi/hashes/${1//\//_}.log")
+            if [ ${stat} -eq 0 ]
+            then
+               # mark as successful - skip next 24h
+               if [ ${unchanged} -ne 0 ]
+               then 
+                  output "- mark as unchanged."
+                  HASHER_DELTA_LAST_OK["${RUNTIME["BACKUP_ROOT"]}/backup.avahi/hashes/${1//\//_}"]=$(date +%s)
+                  output "- write ${#HASHER_DELTA_LAST_OK[@]} caches"
+                  declare -p HASHER_DELTA_LAST_OK > "${RUNTIME["BACKUP_ROOT"]}/backup.avahi/hashes/.cache"
+                  return 0
+               else
+                  # there was patching
+                  return 1
+               fi
+            else
+               return 1
+            fi
          # 4. apply patch
          # 5. cleanup
+         else
+            # mismatch in version
+            return 2
+         fi
       else
-         # mismatch in version
-         return 2
+         output "- skipped as last_ok < 24h"
+         return 0
       fi
    else
       # local hash not complete
